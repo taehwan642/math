23c0845885d185aa311992b9e8cb2f380c0532cf
0ddarri <eungania88@gmail.com>
2023-03-27 23:52:55 +0900

    Add Vector3 +-*/ operator, Dot, Cross

--- Vector.cpp
+++ Vector.cpp
@@ -1,6 +1,19 @@
 #include "Vector.h"
 
-float Math::Vector::GetX()
+Math::Vector3::Vector3(float x, float y, float z)
 {
-    return x;
+    this->x = x;
+    this->y = y;
+    this->z = z;
 }
+
+float Math::Vector3::Dot(Vector3& v1, Vector3& v2)
+{
+    return (v1.x * v1.x + v1.y * v1.y + v1.z * v1.z);
+}
+
+Math::Vector3 Math::Vector3::Cross(Vector3& v1, Vector3& v2)
+{
+    return Vector3(v1.y * v2.z - v1.z * v2.y, v1.z * v2.x - v1.x * v2.z, v1.x * v2.y - v1.y * v2.x);
+}
+
--- includes/Vector.h
+++ includes/Vector.h
@@ -1,10 +1,40 @@
+#pragma once
+
 namespace Math
 {
-    class Vector
+    class Vector3
     {
+    private:
+
     public:
         float x;
+        float y;
+        float z;
 
-        float GetX();
+        Vector3(float x, float y, float z);
+        
+        static float Dot(Vector3& v1, Vector3& v2);
+        static Vector3 Cross(Vector3& v1, Vector3& v2);
+
+        Vector3* operator+(Vector3& ref)
+        {
+            return new Vector3(this->x + ref.x, this->y + ref.y, this->z + ref.z);
+        }
+
+        Vector3* operator-(Vector3& ref)
+        {
+            return new Vector3(this->x - ref.x, this->y - ref.y, this->z - ref.z);
+        }
+
+        Vector3* operator*(Vector3& ref)
+        {
+            return new Vector3(this->x * ref.x, this->y * ref.y, this->z * ref.z);
+        }
+
+        Vector3* operator/(Vector3& ref)
+        {
+            return new Vector3(this->x / ref.x, this->y / ref.y, this->z / ref.z);
+        }
+
     };
 }
