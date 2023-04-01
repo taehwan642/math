a6189babe4b0d4ce8ce7c319de3d75d4b5dac915
0ddarri <eungania88@gmail.com>
2023-04-01 20:57:09 +0900

    Edit Vector

--- Vector.cpp
+++ Vector.cpp
@@ -1,35 +1,6 @@
 #include "Vector.h"
-#include <cmath>
 
-Math::Vector3::Vector3(float x, float y, float z)
+float Math::Vector::GetX()
 {
-    this->x = x;
-    this->y = y;
-    this->z = z;
-}
-
-float Math::Vector3::Dot(const Vector3& v1, const Vector3& v2)
-{
-    return (v1.x * v1.x + v1.y * v1.y + v1.z * v1.z);
-}
-
-Math::Vector3 Math::Vector3::Cross(const Vector3& v1, const Vector3& v2)
-{
-    return Vector3(v1.y * v2.z - v1.z * v2.y, v1.z * v2.x - v1.x * v2.z, v1.x * v2.y - v1.y * v2.x);
-}
-
-float Math::Vector3::Length(const Vector3& v)
-{
-    return sqrt(v.x * v.x + v.y * v.y + v.z * v.z);
-}
-
-float Math::Vector3::LengthSq(const Vector3& v)
-{
-    return v.x * v.x + v.y * v.y + v.z * v.z;
-}
-
-Math::Vector3 Math::Vector3::Normalize(const Vector3& v)
-{
-    float len = Length(v);
-    return Vector3(v.x / len, v.y / len, v.z / len);
+    return x;
 }
--- includes/Vector.h
+++ includes/Vector.h
@@ -1,43 +1,10 @@
-#pragma once
-
 namespace Math
 {
-    class Vector3
+    class Vector
     {
-    private:
-
     public:
         float x;
-        float y;
-        float z;
 
-        Vector3(float x, float y, float z);
-        
-        static float Dot(const Vector3& v1, const Vector3& v2);
-        static Vector3 Cross(const Vector3& v1, const Vector3& v2);
-        static float Length(const Vector3& v);
-        static float LengthSq(const Vector3& v);
-        static Vector3 Normalize(const Vector3& v);
-
-        Vector3* operator+(const Vector3& ref)
-        {
-            return new Vector3(this->x + ref.x, this->y + ref.y, this->z + ref.z);
-        }
-
-        Vector3* operator-(const Vector3& ref)
-        {
-            return new Vector3(this->x - ref.x, this->y - ref.y, this->z - ref.z);
-        }
-
-        Vector3* operator*(const Vector3& ref)
-        {
-            return new Vector3(this->x * ref.x, this->y * ref.y, this->z * ref.z);
-        }
-
-        Vector3* operator/(const Vector3& ref)
-        {
-            return new Vector3(this->x / ref.x, this->y / ref.y, this->z / ref.z);
-        }
-
+        float GetX();
     };
 }
